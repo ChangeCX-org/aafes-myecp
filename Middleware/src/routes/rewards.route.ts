a2a43a6e8c420b06a8de55b6e41352f76ced69b6
Ridhu1808 <ridhu.varshini@changecx.com>
2023-12-08 15:47:54 +0530

    Ridhu | Swagger UI integration for Offers and rewards

--- Middleware/src/routes/rewards.route.ts
+++ Middleware/src/routes/rewards.route.ts
@@ -10,12 +10,12 @@ const router = express.Router();
  * components: 
  *   securitySchemes:
  *     bearerAuth:
- *         type: http
- *         scheme: bearer
- *         bearerFormat: JWT
- *         in: header
- *         name: Authorization
- *         description: Bearer Token
+ *        type: http
+ *        scheme: bearer
+ *        bearerFormat: JWT
+ *        in: header
+ *        name: Authorization
+ *        description: Bearer Token
  *   
  */
 
@@ -28,80 +28,80 @@ const router = express.Router();
 
 /**
  * @openapi
- * /v1/rewards/card:
- *   get:
- *     summary: Get available rewards
- *     tags: [rewards]
- *     security:
- *       - BearerAuth: []
- *     responses:
- *       '200':
- *         description: Successful response with a list of available rewards.
- *       '401':
- *         description: Rewards not found
+ * '/v1/rewards/card':
+ *    get:
+ *      summary: Get available rewards
+ *      tags: [rewards]
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Successful response with a list of available rewards.
+ *        '401':
+ *          description: Rewards not found
  */
 
 router.get('/card', auth, getAvailableRewards)
 
 /**
  * @openapi
- * /v1/rewards:
- *   post:
- *     summary: Get available offers
- *     tags: [rewards]
- *     security:
- *       - BearerAuth: []
- *     responses:
- *       '200':
- *         description: Successful response with a list of available rewards.
+ * '/v1/rewards':
+ *    post:
+ *      summary: Get available offers
+ *      tags: [rewards]
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Successful response with a list of available rewards.
  */
 
 router.post('/', auth, postRewards);
 
 /**
  * @openapi
- * /v1/rewards/{id}:
- *   patch:
- *     summary: Get offer details
- *     tags: [rewards]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         description: The ID of the reward to retrieve.
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       '200':
- *         description: Successful response with details of the specified rewards.
- *       '401':
- *         description: Rewards not found.
+ * '/v1/rewards/{id}':
+ *    patch:
+ *      summary: Get offer details
+ *      tags: [rewards]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the reward to retrieve.
+ *          required: true
+ *          schema:
+ *            type: integer
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Successful response with details of the specified rewards.
+ *        '401':
+ *          description: Rewards not found.
  */
 
 router.patch("/:id", auth, patchRewards);
 
 /**
  * @openapi
- * /v1/rewards/{id}:
- *   delete:
- *     summary: Get offer details
- *     tags: [rewards]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         description: The ID of the reward to retrieve.
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       '200':
- *         description: Successfully deleted the reward.
- *       '401':
- *         description: Rewards deletion failed.
+ * '/v1/rewards/{id}':
+ *    delete:
+ *      summary: Get offer details
+ *      tags: [rewards]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the reward to retrieve.
+ *          required: true
+ *          schema:
+ *            type: integer
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        '200':
+ *          description: Successfully deleted the reward.
+ *        '401':
+ *          description: Rewards deletion failed.
  */
 
 router.delete("/:id", auth, deleteRewards);
